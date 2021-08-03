cdf4b487b32688a06b784c41266ade1a9ef2c1f1
lithial <jameshessell@gmail.com>
2021-08-03 13:08:29 +1200

    feat:Responsive design for the projects tab

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -4,13 +4,12 @@ import {
     Card,
     CardContent,
     CardHeader,
-    CardMedia, Divider, Icon, Link,
+    CardMedia, Divider,
     List,
     ListItem, ListItemIcon, ListItemText,
     makeStyles, SvgIcon,
     Typography
 } from "@material-ui/core";
-import {red} from "@material-ui/core/colors";
 import {ReactComponent as github} from "../assets/icons/github.svg"
 import {ReactComponent as atlas} from "../assets/icons/iconmonstr-globe-3.svg"
 import Button from "@material-ui/core/Button";
@@ -19,8 +18,9 @@ import Button from "@material-ui/core/Button";
 const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 1024,
-        minWidth: 420,
+        minWidth: 280,
         margin: "auto",
+
     },
     spacer: {
         height: "1rem"
@@ -37,18 +37,10 @@ const useStyles = makeStyles((theme) => ({
         height: 0,
         paddingTop: '56.25%', // 16:9
         flex: "1",
-        [theme.breakpoints.down('md')]:{
-            paddingRight: "10px",
-            paddingLeft: "10px",
-            flexShrink:"50%",
-        }
+
     },
     content: {
         flex: "1",
-        [theme.breakpoints.down('md')]:{
-            paddingRight: "20px",
-            paddingLeft: "20px",
-        }
     },
     svgIcon:{
       width: "2.5em",
@@ -63,24 +55,37 @@ const useStyles = makeStyles((theme) => ({
             paddingLeft: "0",
             paddingRight: "0",
             flexDirection:"row",
-            flex: "0.2",
-            flexGrow: "0",
-            //flexFlow:" row wrap",
-            alignContent: "space-between",
-            justifyContent: "space-between",
-
+            width: "100%",
         },
         [theme.breakpoints.down('sm')]:{
             paddingLeft: "0",
+            paddingRight: "0",
             flexDirection:"row",
         }
     },
+    listItem:{
+        [theme.breakpoints.down('md')]:{
+            //flexWrap: "wrap",
+            display: "block",
+        },
+        [theme.breakpoints.down('xs')]:{
+            //flexWrap: "wrap",
+            display: "inline-block",
+            //outline:"1px solid red",
+            width: "13vw",
+        },
+    },
+
     listIcon:{
 
     },
     listText:{
 
+    },
+    lineBreak:{
+        display: "hidden",
     }
+
 
 }));
 
@@ -166,8 +171,9 @@ const ProjectCards = (props) => {
                                     <List className={classes.list} dense={true}>
                                         {
                                             project.techDescription.list.map((listItem, index) => {
+
                                                 return (
-                                                    <ListItem key={index}>
+                                                    <ListItem className={classes.listItem} key={index}>
                                                         <ListItemIcon className={classes.listIcon}>
                                                             <SvgIcon component={listItem.icon}/>
                                                         </ListItemIcon>
